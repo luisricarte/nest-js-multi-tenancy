1744af13def540063bcbbe1d438917a051685358
Luis Ricarte <luis.ricarte@ccc.ufcg.edu.br>
2024-06-19 20:35:38 -0300

    events - create | tenant - interceptor

---
+++ src/events/events.controller.ts
@@ -0,0 +1,48 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { EventsService } from './events.service';
+import { CreateEventDto } from './dto/create-event.dto';
+import { UpdateEventDto } from './dto/update-event.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { TenantInterceptor } from 'src/tenant/tenant.interceptor';
+
+@UseInterceptors(TenantInterceptor)
+@UseGuards(AuthGuard)
+@Controller('events')
+export class EventsController {
+  constructor(private readonly eventsService: EventsService) {}
+
+  @Post()
+  async create(@Body() createEventDto: CreateEventDto) {
+    return this.eventsService.create(createEventDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.eventsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.eventsService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateEventDto: UpdateEventDto) {
+    return this.eventsService.update(+id, updateEventDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.eventsService.remove(+id);
+  }
+}
