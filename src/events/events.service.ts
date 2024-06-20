769314cfa86b5c97c0dadce0848318d7f851f445
Luis Ricarte <luis.ricarte@ccc.ufcg.edu.br>
2024-06-19 21:01:27 -0300

    add RolesGuard

--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { CreateEventDto } from './dto/create-event.dto';
-import { UpdateEventDto } from './dto/update-event.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { TenantService } from 'src/tenant/tenant.service';
 
@@ -32,13 +31,5 @@ export class EventsService {
 
   findOne(id: number) {
     return `This action returns a #${id} event`;
-  }
-
-  update(id: number, updateEventDto: UpdateEventDto) {
-    return `This action updates a #${id} event`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} event`;
   }
 }
